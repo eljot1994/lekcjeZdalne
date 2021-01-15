8b736420ba203cfc40dd2d4d3d39b4b88208a51a
Baaaarteq <baaaarteq@gmail.com>
2021-01-15 19:25:28 +0100

    zad5

--- zad5.py
+++ zad5.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import math
 
 print('5.1')
 kraina = []
@@ -7,21 +8,54 @@ with open('kraina.txt') as dane:
         kraina.append([linia.split(';')[i] for i in range(0,len(linia.split(';')))])
     dane.close()
 suma = [0,0,0,0]
+licznik = [0,0,0,0]
 for woj in kraina:
     region = woj[0][-1]
     if region == 'A':
         suma[0] += int(woj[1]) + int(woj[2])
+        if woj[1] < woj[3] and woj[2] < woj[4]:
+            licznik[0] += 1
     if region == 'B':
         suma[1] += int(woj[1]) + int(woj[2])
+        if woj[1] < woj[3] and woj[2] < woj[4]:
+            licznik[1] += 1
     if region == 'C':
         suma[2] += int(woj[1]) + int(woj[2])
+        if woj[1] < woj[3] and woj[2] < woj[4]:
+            licznik[2] += 1
     if region == 'D':
         suma[3] += int(woj[1]) + int(woj[2])
+        if woj[1] < woj[3] and woj[2] < woj[4]:
+            licznik[3] += 1
 print(suma)
 plt.bar([1,2,3,4],suma)
 plt.xticks([1,2,3,4],('A','B','C','D'))
 plt.xlabel('Region')
 plt.ylabel('Ludność')
 plt.ylim(33*10**6,58*10**6)
-plt.show()
+#plt.show()
+
+print('5.2')
+#print(licznik, sum(licznik))
+print('Kraj: ', sum(licznik), ' \nA:', licznik[0], 'B:', licznik[1], 'C:', licznik[2], 'D:', licznik[3])
+#print('Kraj: %d \nA: %d' % (sum(licznik), licznik[0]))
+
+print('5.3')
+#ludnosc do roku 2025
+rok = 2025
+rok_p = 2013
+ludnosc_t = []
+licznik_p = 0
+for woj in kraina:
+    tempo = int(10000 * ((int(woj[3]) + int(woj[4]))/(int(woj[1]) + int(woj[2])))) / 10000
+    ludnosc = int(woj[3]) + int(woj[4])
+    for _ in range(rok_p + 2, rok + 1, 1):
+        ludnosc = int(ludnosc * tempo)
+        if ludnosc > 2 * (int(woj[1]) + int(woj[2])):
+            licznik_p += 1
+            break
+    ludnosc_t.append([woj[0], ludnosc, tempo])
+print(ludnosc_t)
+print(licznik_p)
+#sprawdzic, gdzie jest najw ludnosc i zsumowac
 
